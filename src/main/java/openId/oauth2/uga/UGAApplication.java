66581cf72386a81cf3018e8809822a601b6df881
1InfinityDoesExist <patelavinash508@gmail.com>
2021-04-24 02:27:55 +0545

    boiler plate

---
+++ src/main/java/openId/oauth2/uga/UGAApplication.java
@@ -0,0 +1,13 @@
+package openId.oauth2.uga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UGAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UGAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/openId/oauth2/uga/UGAApplicationTests.java
@@ -0,0 +1,13 @@
+package openId.oauth2.uga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UGAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
